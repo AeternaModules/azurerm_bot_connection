d585cf0246808df73e4f972607a7dc906fd099b5
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:17 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,43 +1,38 @@
-output "bot_connections" {
-  description = "All bot_connection resources"
-  value       = azurerm_bot_connection.bot_connections
-  sensitive   = true
-}
 output "bot_connections_bot_name" {
-  description = "List of bot_name values across all bot_connections"
-  value       = [for k, v in azurerm_bot_connection.bot_connections : v.bot_name]
+  description = "Map of bot_name values across all bot_connections, keyed the same as var.bot_connections"
+  value       = { for k, v in azurerm_bot_connection.bot_connections : k => v.bot_name }
 }
 output "bot_connections_client_id" {
-  description = "List of client_id values across all bot_connections"
-  value       = [for k, v in azurerm_bot_connection.bot_connections : v.client_id]
+  description = "Map of client_id values across all bot_connections, keyed the same as var.bot_connections"
+  value       = { for k, v in azurerm_bot_connection.bot_connections : k => v.client_id }
 }
 output "bot_connections_client_secret" {
-  description = "List of client_secret values across all bot_connections"
-  value       = [for k, v in azurerm_bot_connection.bot_connections : v.client_secret]
+  description = "Map of client_secret values across all bot_connections, keyed the same as var.bot_connections"
+  value       = { for k, v in azurerm_bot_connection.bot_connections : k => v.client_secret }
   sensitive   = true
 }
 output "bot_connections_location" {
-  description = "List of location values across all bot_connections"
-  value       = [for k, v in azurerm_bot_connection.bot_connections : v.location]
+  description = "Map of location values across all bot_connections, keyed the same as var.bot_connections"
+  value       = { for k, v in azurerm_bot_connection.bot_connections : k => v.location }
 }
 output "bot_connections_name" {
-  description = "List of name values across all bot_connections"
-  value       = [for k, v in azurerm_bot_connection.bot_connections : v.name]
+  description = "Map of name values across all bot_connections, keyed the same as var.bot_connections"
+  value       = { for k, v in azurerm_bot_connection.bot_connections : k => v.name }
 }
 output "bot_connections_parameters" {
-  description = "List of parameters values across all bot_connections"
-  value       = [for k, v in azurerm_bot_connection.bot_connections : v.parameters]
+  description = "Map of parameters values across all bot_connections, keyed the same as var.bot_connections"
+  value       = { for k, v in azurerm_bot_connection.bot_connections : k => v.parameters }
 }
 output "bot_connections_resource_group_name" {
-  description = "List of resource_group_name values across all bot_connections"
-  value       = [for k, v in azurerm_bot_connection.bot_connections : v.resource_group_name]
+  description = "Map of resource_group_name values across all bot_connections, keyed the same as var.bot_connections"
+  value       = { for k, v in azurerm_bot_connection.bot_connections : k => v.resource_group_name }
 }
 output "bot_connections_scopes" {
-  description = "List of scopes values across all bot_connections"
-  value       = [for k, v in azurerm_bot_connection.bot_connections : v.scopes]
+  description = "Map of scopes values across all bot_connections, keyed the same as var.bot_connections"
+  value       = { for k, v in azurerm_bot_connection.bot_connections : k => v.scopes }
 }
 output "bot_connections_service_provider_name" {
-  description = "List of service_provider_name values across all bot_connections"
-  value       = [for k, v in azurerm_bot_connection.bot_connections : v.service_provider_name]
+  description = "Map of service_provider_name values across all bot_connections, keyed the same as var.bot_connections"
+  value       = { for k, v in azurerm_bot_connection.bot_connections : k => v.service_provider_name }
 }
 
